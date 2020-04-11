546f01987df4ae4143da010e27141a7e2787ba93
AshLePoney <ashara.web@gmail.com>
2020-04-11 23:24:02 +0200

    test: add tests for registration method and added defs

--- test/specs/src/index.spec.js
+++ test/specs/src/index.spec.js
@@ -1,6 +1,111 @@
+const defaultAssert = require('assert');
 const path = require('path');
-const main = require(path.resolve(__root, 'src/index.js'));
+const enhancedAssert = require(path.resolve(__root, 'src/index.js'));
+const definitions = require(path.resolve(__root, 'src/definitions.js'));
 
-test('Should not be null', () => {
-  expect(main).not.toBe(null);
+describe('module.exports (enhanced-assert)', () => {
+  test('should exported module includes _utils methods object.', () => {
+    expect(enhancedAssert).toHaveProperty('_utils');
+    expect(enhancedAssert._utils).toHaveProperty('register');
+    expect(enhancedAssert._utils).toHaveProperty('registerStandardAssert');
+    expect(enhancedAssert._utils).toHaveProperty('registerOptionalAssert');
+    expect(enhancedAssert._utils).toHaveProperty('registerArrayOfAssert');
+    expect(enhancedAssert._utils).toHaveProperty('registerOptionalArrayOfAssert');
+    expect(enhancedAssert._utils).toHaveProperty('getClassname');
+    expect(enhancedAssert._utils).toHaveProperty('getTypeof');
+  });
+
+  test('should exported module includes default Node.js assert props', () => {
+    Object.entries(defaultAssert).forEach(([name, assertion]) => {
+      expect(enhancedAssert[name]).toBe(assertion);
+    });
+  });
+
+  test('should exported module includes added assert props', () => {
+    Object.keys(definitions).forEach((name) => {
+      expect(enhancedAssert[name]).toBeInstanceOf(Function);
+    });
+  });
+});
+
+describe('assert._utils', () => {
+  const utils = enhancedAssert._utils;
+
+  describe('register', () => {
+    test('throw an error when the "target" argument is not valid', () => {
+      expect(() => utils.register(0, {}, {}))
+        .toThrowError('The "target" argument must be an object or function.');
+    });
+
+    test('throw an error when the "definitions" argument is not valid', () => {
+      expect(() => utils.register({}, null, {}))
+        .toThrowError('The "definitions" argument must be an object.');
+    });
+
+    test('throw an error when the "options" argument is not valid', () => {
+      expect(() => utils.register({}, {}, null))
+        .toThrowError('The "options" argument must be an object.');
+    });
+
+    test('should return the provided target', () => {
+      const defs = {
+        custom: {
+          check: (arg) => (typeof arg === 'boolean'),
+          operator: 'strictEqual',
+          actual: (arg) => typeof arg
+        }
+      };
+      const ref = {};
+
+      const ret = utils.register(ref, defs);
+
+      expect(ref).toBe(ret);
+    });
+
+    test('should return the default exported module when "target" argument is not provided', () => {
+      const testID = '__test__.module._utils.register[0]';
+      const defs = {
+        [testID]: {
+          check: (arg) => (typeof arg === 'boolean'),
+          operator: 'strictEqual',
+          actual: (arg) => typeof arg
+        }
+      };
+
+      const ret = utils.register(null, defs);
+
+      expect(ret).toBe(enhancedAssert);
+    });
+
+    test('should not register method when options specify not', () => {
+      const ret = utils.register({}, { custom: {} }, {
+        standard: false,
+        optional: false,
+        arrayOf: false,
+        optionalArrayOf: false
+      });
+
+      expect(ret).not.toHaveProperty('custom');
+      expect(ret).not.toHaveProperty('optionalCustom');
+      expect(ret).not.toHaveProperty('arrayOfCustom');
+      expect(ret).not.toHaveProperty('optionalArrayOfCustom');
+    });
+
+    test('should register method', () => {
+      const defs = {
+        custom: {
+          check: (arg) => (typeof arg === 'boolean'),
+          operator: 'strictEqual',
+          actual: (arg) => typeof arg
+        }
+      };
+
+      const ret = utils.register({}, defs);
+
+      expect(ret).toHaveProperty('custom');
+      expect(ret).toHaveProperty('optionalCustom');
+      expect(ret).toHaveProperty('arrayOfCustom');
+      expect(ret).toHaveProperty('optionalArrayOfCustom');
+    });
+  });
 });
---
+++ test/specs/src/registered.spec.js
@@ -0,0 +1,413 @@
+/* eslint-disable no-array-constructor, no-new-func */
+
+const path = require('path');
+const { Stream, Readable, Writable, Duplex, Transform } = require('stream');
+const enhancedAssert = require(path.resolve(__root, 'src/index.js'));
+const { EnhancedAssertionError } = require(path.resolve(__root, 'src/errors.js'));
+const { capitalize } = require(path.resolve(__root, 'src/utils.js'));
+
+const stream = new Stream();
+const readableStream = new Readable({ _read: () => {} });
+const writableStream = new Writable({ _write: () => {} });
+const duplexStream = new Duplex();
+const transformStream = new Transform({ _transform: () => {} });
+
+const entries = {
+  // Primitives types.
+  bool: {
+    valid: [
+      false,
+      true
+    ],
+    invalid: [
+      -1,
+      0,
+      1,
+      'abc',
+      /regexp/,
+      {},
+      [],
+      ['abc'],
+      function () {},
+      async function () {},
+      Symbol('__test__'),
+      new Promise(() => {})
+    ]
+  },
+  number: {
+    valid: [
+      -1,
+      0,
+      1
+    ],
+    invalid: [
+      false,
+      true,
+      'abc',
+      /regexp/,
+      {},
+      [],
+      ['abc'],
+      function () {},
+      async function () {},
+      Symbol('__test__'),
+      new Promise(() => {})
+    ]
+  },
+  string: {
+    valid: [
+      '',
+      'abc',
+      String('abc')
+    ],
+    invalid: [
+      false,
+      true,
+      -1,
+      0,
+      1,
+      /regexp/,
+      {},
+      [],
+      ['abc'],
+      function () {},
+      async function () {},
+      Symbol('__test__'),
+      new Promise(() => {})
+    ]
+  },
+  symbol: {
+    valid: [
+      Symbol('__test__'),
+      Symbol.for('__test__')
+    ],
+    invalid: [
+      false,
+      true,
+      -1,
+      0,
+      1,
+      'abc',
+      /regexp/,
+      {},
+      [],
+      ['abc'],
+      function () {},
+      async function () {},
+      new Promise(() => {})
+    ]
+  },
+
+  // No-primitives types.
+  object: {
+    valid: [
+      {},
+      Object(),
+      Object.create(null)
+    ],
+    invalid: [
+      false,
+      true,
+      -1,
+      0,
+      1,
+      'abc',
+      function () {},
+      async function () {},
+      Symbol('__test__')
+    ]
+  },
+  function: {
+    valid: [
+      function () {},
+      async function () {},
+      Function()
+    ],
+    invalid: [
+      false,
+      true,
+      -1,
+      0,
+      1,
+      'abc',
+      /regexp/,
+      {},
+      [],
+      ['abc'],
+      Symbol('__test__'),
+      new Promise(() => {})
+    ]
+  },
+
+  // Javascript types.
+  array: {
+    valid: [
+      [],
+      ['abc'],
+      Array()
+    ],
+    invalid: [
+      false,
+      true,
+      -1,
+      0,
+      1,
+      'abc',
+      /regexp/,
+      {},
+      function () {},
+      async function () {},
+      Symbol('__test__'),
+      new Promise(() => {})
+    ]
+  },
+  asyncFunction: {
+    valid: [
+      async function () {}
+    ],
+    invalid: [
+      false,
+      true,
+      -1,
+      0,
+      1,
+      'abc',
+      /regexp/,
+      {},
+      [],
+      ['abc'],
+      function () {},
+      Symbol('__test__'),
+      new Promise(() => {})
+    ]
+  },
+  promise: {
+    valid: [
+      new Promise(() => {}),
+      (async () => {})()
+    ],
+    invalid: [
+      false,
+      true,
+      -1,
+      0,
+      1,
+      'abc',
+      /regexp/,
+      {},
+      [],
+      ['abc'],
+      function () {},
+      async function () {},
+      Symbol('__test__')
+    ]
+  },
+  date: {
+    valid: [
+      new Date()
+    ],
+    invalid: [
+      false,
+      true,
+      -1,
+      0,
+      1,
+      'abc',
+      /regexp/,
+      {},
+      [],
+      ['abc'],
+      function () {},
+      async function () {},
+      new Promise(() => {})
+    ]
+  },
+  regexp: {
+    valid: [
+      /regexp/,
+      new RegExp('')
+    ],
+    invalid: [
+      false,
+      true,
+      -1,
+      0,
+      1,
+      'abc',
+      {},
+      [],
+      ['abc'],
+      function () {},
+      async function () {},
+      Symbol('__test__'),
+      new Promise(() => {})
+    ]
+  },
+
+  // Node.js types.
+  buffer: {
+    valid: [
+      Buffer.from('abc')
+    ],
+    invalid: [
+      false,
+      true,
+      -1,
+      0,
+      1,
+      'abc',
+      /regexp/,
+      {},
+      [],
+      ['abc'],
+      function () {},
+      async function () {},
+      Symbol('__test__'),
+      new Promise(() => {})
+    ]
+  },
+  stream: {
+    valid: [
+      stream,
+      readableStream,
+      writableStream,
+      duplexStream,
+      transformStream
+    ],
+    invalid: [
+      false,
+      true,
+      -1,
+      0,
+      1,
+      'abc',
+      /regexp/,
+      {},
+      [],
+      ['abc'],
+      function () {},
+      async function () {},
+      Symbol('__test__'),
+      new Promise(() => {})
+    ]
+  },
+  readable: {
+    valid: [
+      readableStream,
+      duplexStream,
+      transformStream
+    ],
+    invalid: [
+      writableStream,
+      false,
+      true,
+      -1,
+      0,
+      1,
+      'abc',
+      /regexp/,
+      {},
+      [],
+      ['abc'],
+      function () {},
+      async function () {},
+      Symbol('__test__'),
+      new Promise(() => {})
+    ]
+  },
+  writable: {
+    valid: [
+      writableStream,
+      duplexStream,
+      transformStream
+    ],
+    invalid: [
+      readableStream,
+      false,
+      true,
+      -1,
+      0,
+      1,
+      'abc',
+      /regexp/,
+      {},
+      [],
+      ['abc'],
+      function () {},
+      async function () {},
+      Symbol('__test__'),
+      new Promise(() => {})
+    ]
+  },
+  duplex: {
+    valid: [
+      duplexStream,
+      transformStream
+    ],
+    invalid: [
+      readableStream,
+      writableStream,
+      false,
+      true,
+      -1,
+      0,
+      1,
+      'abc',
+      /regexp/,
+      {},
+      [],
+      ['abc'],
+      function () {},
+      async function () {},
+      Symbol('__test__'),
+      new Promise(() => {})
+    ]
+  }
+};
+
+Object.entries(entries).forEach(([name, type]) => {
+  const capitalizedName = capitalize(name);
+
+  test(`assert.${name}`, () => {
+    type.valid.forEach((value) => {
+      expect(() => enhancedAssert[name](value)).not.toThrow();
+    });
+    expect(() => enhancedAssert[name](null)).toThrowError(EnhancedAssertionError);
+    expect(() => enhancedAssert[name](undefined)).toThrowError(EnhancedAssertionError);
+    type.invalid.forEach((value) => {
+      expect(() => enhancedAssert[name](value)).toThrowError(EnhancedAssertionError);
+    });
+  });
+
+  test(`assert.optional${capitalizedName}`, () => {
+    const key = `optional${capitalizedName}`;
+
+    expect(() => enhancedAssert[key](null)).not.toThrow();
+    expect(() => enhancedAssert[key](undefined)).not.toThrow();
+    type.valid.forEach((value) => {
+      expect(() => enhancedAssert[key](value)).not.toThrow();
+    });
+    type.invalid.forEach((value) => {
+      expect(() => enhancedAssert[key](value)).toThrowError(EnhancedAssertionError);
+    });
+  });
+
+  test(`assert.arrayOf${capitalizedName}`, () => {
+    const key = `arrayOf${capitalizedName}`;
+
+    expect(() => enhancedAssert[key](type.valid)).not.toThrow();
+    expect(() => enhancedAssert[key](null)).toThrowError(EnhancedAssertionError);
+    expect(() => enhancedAssert[key](undefined)).toThrowError(EnhancedAssertionError);
+    expect(() => enhancedAssert[key](type.invalid)).toThrowError(EnhancedAssertionError);
+  });
+
+  test(`assert.optionalArrayOf${capitalizedName}`, () => {
+    const key = `optionalArrayOf${capitalizedName}`;
+
+    expect(() => enhancedAssert[key](null)).not.toThrow();
+    expect(() => enhancedAssert[key](undefined)).not.toThrow();
+    expect(() => enhancedAssert[key](type.valid)).not.toThrow();
+    expect(() => enhancedAssert[key]({})).toThrowError(EnhancedAssertionError);
+    expect(() => enhancedAssert[key](type.invalid)).toThrowError(EnhancedAssertionError);
+  });
+});
